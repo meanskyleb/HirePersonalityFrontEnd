9e787d72955a9824f306db3516a6b4ec0f4201d7
zackigoldberg <zackigoldberg@gmail.com>
2019-01-10 13:19:19 -0500

    Login component working

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -33,6 +33,6 @@ export class RegistrationComponent implements OnInit {
     console.log(this._registerForm.value);
     this._authService
     .register(this._registerForm.value)
-    .subscribe( () => console.log('you did it'));
+    .subscribe( () => this._authService.login(this._registerForm.value));
   }
 }
