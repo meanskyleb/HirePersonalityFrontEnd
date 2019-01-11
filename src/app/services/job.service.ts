8bdfaf7f2c45f5c8a67a7000992b48f45de4fd62
zjmcdon98 <zjmcdon98@gmail.com>
2019-01-11 12:22:49 -0500

    Added Job Service

---
+++ src/app/services/job.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const ApiUrl = "https://localhost:44311/api/values"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobService {
+
+  constructor(private _http: HttpClient) { }
+
+  getJobs() {
+    return this._http.get(`${ApiUrl}/Jobs`, {headers: this.getHeaders() });
+  }
+
+  private getHeaders() {
+    return new HttpHeaders().set('Authorization', `Bearer $(localStorage.getItem('id_token')}`)
+  }
+}
